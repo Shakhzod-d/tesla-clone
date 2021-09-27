23247a7859100a3a4b4f3843c0e497b8dda07664
Shakhzod <forworkingappsinmylaptop@gmail.com>
2021-09-27 13:13:47 +0500

    ADD: redux for cars

--- tesla-clone/src/App.js
+++ tesla-clone/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 
 function Section({
   title,
@@ -10,17 +11,21 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
-      <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
-        <DownArrow src="./images/down-arrow.svg" />
-      </Buttons>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+      <Fade bottom>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+          <DownArrow src="./images/down-arrow.svg" />
+        </Buttons>
+      </Fade>
     </Wrap>
   );
 }
@@ -28,6 +33,7 @@ function Section({
 export default Section;
 
 const Wrap = styled.div`
+  z-index: 10;
   width: 100vw;
   height: 100vh;
   background-size: cover;
